16b37bcdb690d5d0ba903d702f1b80b1670d7843
mj-anarchist <mujeeb.ishaque@gmail.com>
2022-03-28 01:52:02 +0200

    add and delete methods

--- core.py
+++ core.py
@@ -9,8 +9,8 @@ class Manager:
     database_name = 'records.json'
         
     def __init__(self):
-        connection = TinyDB(self.current_directory  + self.database_name)
-        connection_str = str(connection).split(',')
+        self.connection = TinyDB(self.current_directory  + self.database_name)
+        connection_str = str(self.connection).split(',')
         _, _, tail = connection_str[1].partition('=')
         if int(tail) == 0:
             return Alerts.show_alert('error', "ERROR: Can't connect to the local database. ")
@@ -19,7 +19,13 @@ class Manager:
     def current_directory(self):
         return os.path.dirname(os.path.realpath(sys.argv[0]))
     
+    @staticmethod
+    def add_record(text):
+        pass
     
+    @staticmethod
+    def delete_record(record_id):
+        pass
     
 if __name__ == '__main__':
     man = Manager()
