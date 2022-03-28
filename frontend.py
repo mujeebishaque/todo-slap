9942da457e3b65670fc222f9fddb7bae6ad80b15
mj-anarchist <mujeeb.ishaque@gmail.com>
2022-03-28 03:23:14 +0200

    final touches

--- frontend.py
+++ frontend.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from core import Manager
 from utils import Alerts
+
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
